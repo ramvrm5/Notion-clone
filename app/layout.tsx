da5e42d347514d549e8cbfb1e444db122f042d41
ram verma <ramvrm5@gmail.com>
2024-02-06 16:30:30 +0530

    chnages in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
-
 import { Inter } from "next/font/google";
-
 import "./globals.css";
-
 import { ThemeProvider } from "@/components/providers/theme-provider";
-
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });

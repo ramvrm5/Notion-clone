ad51654179baf1cf35796c4df2b895c175329fbc
ram verma <ramvrm5@gmail.com>
2024-05-07 15:40:53 +0530

    add logout option

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -10,7 +10,6 @@ export const UserItem = () => {
 
     return (
         <DropdownMenu>
-            
             <DropdownMenuTrigger asChild>
                 <div role="button" className="flex items-center text-sm p-3 w-full hover:bg-primary/5">
                     <div className="gap-x-2 flex items-center max-w-[150px]">
@@ -26,7 +25,6 @@ export const UserItem = () => {
                     <ChevronsLeftRight className="rotate-90 ml-2 text-muted-foreground h-4 w-4" />
                 </div>
             </DropdownMenuTrigger>
-
             <DropdownMenuContent
                 className="w-80" align="start" alignOffset={11} forceMount
             >

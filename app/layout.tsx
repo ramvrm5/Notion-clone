69bfddafb6f578a706dc1d0fb918d2fc18b3e8a4
ram verma <ramvrm5@gmail.com>
2024-05-24 16:30:30 +0530

    changes in imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
+
 import "./globals.css";
+
 import { ThemeProvider } from "@/components/providers/theme-provider";
+
 import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });

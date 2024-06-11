bbb218b1ad66e51cef243dba7f63ebcdd5225fcb
ram verma <ramvrm5@gmail.com>
2024-06-11 17:50:49 +0530

    changes for navigation

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -77,9 +77,7 @@ export const Navigation = () => {
     const collapse = () => {
         if(sidebarRef.current && navbarRef.current) {
             setIsCollapsed(true);
-            
             setIsResetting(true);
-
             sidebarRef.current.style.width = "0";
             navbarRef.current.style.setProperty("width", "100%");
             navbarRef.current.style.setProperty("left", "0");

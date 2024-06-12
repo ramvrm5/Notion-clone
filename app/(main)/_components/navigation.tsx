2db761110acf37a0620a0105dcd6154a9d01ef11
ram verma <ramvrm5@gmail.com>
2024-06-12 12:17:48 +0530

    changes for get and create uerry

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,10 +6,14 @@ import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
+    const documents = useQuery(api.documents.get);
+
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
     const navbarRef = useRef<ElementRef<"div">>(null);
@@ -105,7 +109,10 @@ export const Navigation = () => {
                     <UserItem />
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                   {documents?.map((document) => (
+                    <p
+                    key={document._id} >{document.title}</p>
+                   ))}
                 </div>
                 <div onMouseDown={handleMouseDown} onClick={resetWidth} className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0">
 
--- convex/documents.ts
+++ convex/documents.ts
@@ -3,6 +3,20 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import {Doc, Id} from "./_generated/dataModel";
 
+export const get = query({
+    handler:async(ctx) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity){
+            throw new Error(" Not authenticated")
+        }
+
+        const documents = await ctx.db.query("documents").collect();
+
+        return documents;
+    }
+});
+
 export const create = mutation({
     args: {
         title: v.string(),
@@ -23,6 +37,8 @@ export const create = mutation({
             userId,
             isArchived:false,
             isPublished:false
-        })
+        });
+
+        return document;
     }
 })

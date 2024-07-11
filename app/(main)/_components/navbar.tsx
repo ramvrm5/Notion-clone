756375840436d065733598732efcb48f095b0774
ram verma <ramvrm5@gmail.com>
2024-07-11 11:32:38 +0530

    changes for heading and emoji

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 
 
 interface NvarbarProps {
@@ -22,8 +24,11 @@ export const Navbar = ({
 
     if (document === undefined) {
         return (
-            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full">
+            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center justify-between">
                 <Title.Skeleton />
+                <div className="flex items-center gap-x-2">
+                    <Menu.Skeleton />
+                </div>
             </nav>
         )
     }
@@ -33,17 +38,26 @@ export const Navbar = ({
     }
 
     return (
-        <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
-            {isCollapsed && (
-                <MenuIcon
-                    role="button"
-                    onClick={onResetWidth}
-                    className="h-6 w-6 text-muted-foreground"
-                />
-            )}
-            <div className="flex items-center justify-between wf-full">
-                <Title initialData={document} />
-            </div>
-        </nav>
+        <>
+            <nav className="bg-background dark:bg-[#1f1f1f] px-3 py-2 w-full flex items-center gap-x-4">
+                {isCollapsed && (
+                    <MenuIcon
+                        role="button"
+                        onClick={onResetWidth}
+                        className="h-6 w-6 text-muted-foreground"
+                    />
+                )}
+                <div className="flex items-center justify-between w-full">
+                    <Title initialData={document} />
+                    <div className="flex items-center gap-x-1">
+                        <Menu documentId={document._id} />
+                    </div>
+                </div>
+            </nav>
+            {
+                document?.isArchived && (
+                    <Banner documentId={document._id} />
+                )}
+        </>
     )
 }
